5bba6a4e33cd16463faafee199729826e1d556a4
ajay9644 <167506435+ajay9644@users.noreply.github.com>
2024-06-06 12:19:55 +0530

    feat :Update and rename script.js to storage_assignment/script.js
    Updated few files in storage_assignment
    -Renamed home.html to storage_assignment/home.html
    -Updated and Renamed script.js to storage_assignment/script.js
    - Added style.css

--- storage_assignment/script.js
+++ storage_assignment/script.js
@@ -1,12 +1,10 @@
 function increaseScore() {
   let localScore = localStorage.getItem("localScore");
-  console.log(localScore);
   localScore++;
   localStorage.setItem("localScore", localScore);
   document.getElementById("localScore").textContent = localScore;
 
   let sessionScore = sessionStorage.getItem("sessionScore");
-  console.log(sessionScore);
   sessionScore++;
   sessionStorage.setItem("sessionScore", sessionScore);
   document.getElementById("sessionScore").textContent = sessionScore;
